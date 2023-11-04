b036f529eec0feccae54d9a1c73daeed889419d8
hubertwi1115 <148622055+hubertwi1115@users.noreply.github.com>
2023-11-04 15:27:16 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int wiekInt;
+        double wiekDouble;
+        float wiekFloat;
+
+        Console.Write("Podaj dowolną liczbę nieparzystą jako wiek: ");
+        int wiek = int.Parse(Console.ReadLine());
+
+        if (wiek % 2 == 0)
+        {
+            Console.WriteLine("Wprowadzony wiek nie jest liczbą nieparzystą.");
+            return;
+        }
+
+        wiekInt = wiek;
+        wiekDouble = (double)wiek;
+        wiekFloat = (float)wiek;
+
+        Console.WriteLine($"Wiek jako int: {wiekInt}");
+        Console.WriteLine($"Wiek jako double: {wiekDouble}");
+        Console.WriteLine($"Wiek jako float: {wiekFloat}");
+    }
+}
